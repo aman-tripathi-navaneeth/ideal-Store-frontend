06d0bb6d9de9414d9a4a1c71823c6eb5155e4d75
Navaneeth Tripathi Aman <142510994+aman-tripathi-navaneeth@users.noreply.github.com>
2025-07-25 21:15:33 +0530

    Delete frontend directory

--- frontend/src/utils/auth.js
+++
@@ -1,7 +0,0 @@
-/**
- * Check if the user is authenticated
- * @returns {boolean} True if the user is authenticated, false otherwise
- */
-export function isAuthenticated() {
-  return localStorage.getItem("isAuthenticated") === "true";
-}
